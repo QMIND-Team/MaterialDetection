dd528d9ed7224e7394b8b97fdc5cafb752bea811
Marcus-U <marcusuhthoff@gmail.com>
2019-02-04 14:56:47 -0500

    VGG files included

--- vgg16try1.py
+++ vgg16try1.py
@@ -1,41 +1,13 @@
-# =============================================================================
-# from keras.preprocessing.image import load_img, ImageDataGenerator
-# from keras.preprocessing.image import img_to_array
-# from keras import optimizers
-# from keras.applications.vgg16 import preprocess_input
-# from keras.applications.vgg16 import decode_predictions
-# =============================================================================
+from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.vgg16 import VGG16
 from keras import models
 from keras import layers
 from keras.callbacks import EarlyStopping, ReduceLROnPlateau, ModelCheckpoint, TensorBoard
 # load the model
-# =============================================================================
-# model = VGG16()
-# # load an image from file
-# #throw the file name of the picture below and make sure its in the right directory
-# os.chdir("mats")
-# for file in os.listdir():
-#     print('Working on %s' % file)
-#     image = load_img(file, target_size=(224, 224))
-#     # convert the image pixels to a numpy array
-#     image = img_to_array(image)
-#     # reshape data for the model
-#     image = image.reshape((1, image.shape[0], image.shape[1], image.shape[2]))
-#     # prepare the image for the VGG model
-#     image = preprocess_input(image)
-#     # predict the probability across all output classes
-#     yhat = model.predict(image)
-#     # convert the probabilities to class labels
-#     label = decode_predictions(yhat)
-#     # retrieve the most likely result, e.g. highest probability
-#     label = label[0][0]
-#     # print the classification
-#     print('%s (%.2f%%)' % (label[1], label[2]*100))
-# =============================================================================
 
 # Hey Leon! You're going to have to tweak a couple of things really quick before running this
 # Throw in the path directory to the materials file I sent you:
+
 train_loc = "C:\\Users\\marcu\\learning\\mats\\train"
 test_loc = "C:\\Users\\marcu\\learning\\mats\\test"
 my_base = VGG16(weights='imagenet',include_top = False, input_shape = (224,224,3))
@@ -57,18 +29,22 @@ train_datagen = ImageDataGenerator(
         horizontal_flip = True,
         fill_mode = 'nearest'
         )
+
 test_datagen = ImageDataGenerator(rescale=1./255)
+
 train_generator = train_datagen.flow_from_directory(
         train_loc,
         target_size = (224,224),
         batch_size=20,
         class_mode='categorical'
         )
+
 valid_generator = test_datagen.flow_from_directory(
         test_loc,
         target_size = (224,224),
         batch_size = 20,
         class_mode = 'categorical')
+
 model.compile(loss = 'categorical_crossentropy',
               optimizer = 'rmsprop',
               metrics = ['accuracy'])
@@ -87,10 +63,11 @@ callbacks = [EarlyStopping(monitor='val_loss',
                              save_best_only=True,
                              save_weights_only=True),
              TensorBoard(log_dir='logs')]
+
 history = model.fit_generator(
         train_generator,
         steps_per_epoch = 100,
         epochs = 5,
         validation_data = valid_generator,
-        callbacks = callbacks;
+        callbacks = callbacks,
         validation_steps = 50)
